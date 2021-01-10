1fd6ee17bba0b7f3f93a3fcd03ce4ea6fead0c45
Alexander Pavlov <ap@tsys.pw>
2021-01-10 13:05:31 +0300

    - База: вьютифай, ротер, вуекс, сервер и круд

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
-'use strict';
-
 const Koa = require('koa');
 const Router = require('@koa/router');
+const bodyParser = require('koa-bodyparser');
+
+const cvs = require('./routes/cvs');
 
 const PORT = 3000;
 const HOST = '0.0.0.0';
@@ -9,11 +10,17 @@ const HOST = '0.0.0.0';
 const app = new Koa();
 const router = new Router();
 
-router.get('/api/users/:id', ctx => {
-    ctx.body = ctx.params
-});
+router
+    .post('/api/cv/list', cvs.list)
+    .post('/api/cv/add', cvs.add)
+    .post('/api/cv/update', cvs.update)
+    .post('/api/cv/delete', cvs.delete);
 
 app
+    .use(bodyParser({
+        formLimit: '50mb',
+        jsonLimit: '1mb',
+    }))
     .use(router.routes())
     .use(router.allowedMethods());
 
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import store from './store'
+import router from './router'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
+Vue.config.errorHandler = function (err) {
+  if (vueInstance) {
+    vueInstance.$store.commit('setAppError', err);
+  }
+  let c = console;
+  c.error(err);
+};
+
+
+let vueInstance = new Vue({
+  vuetify,
+  store,
+  router,
+  render: h => h(App)
 }).$mount('#app')
